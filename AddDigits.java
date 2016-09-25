016c985e1fe6497e11375e48bfee990c332fd75b
Nitish Shivrayan <nshivra@ncsu.edu>
2016-09-25 07:32:49 -0400

    Repeatedly add all its digits until the result has only one digit.
     Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
    
    For example:
    
    Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.

--- AddDigits.java
+++ AddDigits.java
@@ -15,3 +15,14 @@ public class Solution {
         return res;
     }
 }
+
+public class Solution {
+    public int addDigits(int num) {
+        if(num == 0)
+            return 0;
+        else if(num%9 == 0)
+            return 9;
+        else
+            return num%9;
+    }
+}
