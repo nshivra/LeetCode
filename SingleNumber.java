c3e3cbbe936d481a08138e328513b1a244b72675
Nitish Shivrayan <nshivra@ncsu.edu>
2016-09-25 06:40:44 -0400

    Finding non-duplicate number in an array
     Given an array of integers, every element appears twice except for one. Find that single one.

---
+++ SingleNumber.java
@@ -0,0 +1,11 @@
+# Given an array of integers, every element appears twice except for one. Find that single one.
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int i=0;i<nums.length;i++){
+            res = res^nums[i];
+        }
+        return res;
+    }
+}
