829bd1d37c308b4baa7edc5c9de51c9427e67e2e
Nitish Shivrayan <nshivra@ncsu.edu>
2016-09-25 05:31:06 -0400

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+// Code for reversing the string
+
+public class Solution {
+    public String reverseString(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        StringBuilder sb = new StringBuilder(s);
+        while(i<j){
+            char c1 = sb.charAt(i);
+            char c2 = sb.charAt(j);
+            sb.setCharAt(i,c2);
+            sb.setCharAt(j,c1);
+            i++;
+            j--;
+        }
+        return sb.toString();
+    }
+}
