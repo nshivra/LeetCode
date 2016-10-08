062439f85acedbdf13c627d4eaad6294efe5349d
Nitish Shivrayan <nshivra@ncsu.edu>
2016-10-08 03:25:38 -0400

    Product of Array Except Self
     Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
    
    Solve it without division and in O(n).
    
    For example, given [1,2,3,4], return [24,12,8,6].

---
+++ ProductArrayExSelf.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = nums[0];
+        for(int i=1;i<nums.length-1;i++){
+            left[i] = left[i-1]*nums[i];
+        }
+        
+        right[nums.length-1] = nums[nums.length-1];
+        for(int i=nums.length-2;i>=0;i--){
+            right[i] = right[i+1]*nums[i];
+        }
+        nums[0] = right[1];
+        nums[nums.length - 1] = left[nums.length-2];
+        for(int i=1;i<nums.length-1;i++){
+            nums[i] = left[i-1] * right[i+1];
+        }
+        
+        return nums;
+    }
+}
