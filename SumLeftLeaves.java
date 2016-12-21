2a8bbc1caabdc88d3e1075c122197ccd25b792ec
Nitish Shivrayan <nshivra@ncsu.edu>
2016-12-21 04:48:04 -0500

    Update SumLeftLeaves.java

--- SumLeftLeaves.java
+++ SumLeftLeaves.java
@@ -9,17 +9,17 @@
  */
 public class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        if(root == null) return 0;        
-        else if(root.left != null && root.right != null){
-		      if(root.left.left == null && root.left.right == null) return root.left.val+sumOfLeftLeaves(root.right);
-		      else return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
-	    }
-        else if(root.left != null && root.right == null){
-		      if(root.left.left == null && root.left.right == null) return root.left.val;
-          else return sumOfLeftLeaves(root.left);
-	    }
-        else if(root.left == null && root.right != null) return sumOfLeftLeaves(root.right);
-        else if(root.left != null && root.right != null) return sumOfLeftLeaves(root.right)+sumOfLeftLeaves(root.left);
-        else return 0;
+        return helper(root, false);
     }
+    
+    private int helper(TreeNode root, boolean isLeft){
+        if(root == null) return 0;
+        if(root.left == null && root.right == null){
+            if(isLeft){
+                return root.val;
+            }
+            return 0;
+        }
+        return helper(root.left, true) + helper(root.right, false);
+    } 
 }
